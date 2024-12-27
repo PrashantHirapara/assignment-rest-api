c6f5ab4780b721045c228153599db50f704b4e0c
Prashant Hirapara <hirapara.prashant08@gmail.com>
2024-12-27 17:18:15 +0530

    Adding update endpoint for employee

--- src/main/java/org/test/service/EmployeeService.java
+++ src/main/java/org/test/service/EmployeeService.java
@@ -49,4 +49,20 @@ public class EmployeeService {
         Department department = departmentRepository.findById(departmentId).orElseThrow(() -> new RuntimeException("Department not found"));
         return department.getEmployees();
     }
+
+    public void updateEmployee(String employeeId, Employee updatedEmployee) {
+        if (employeeRepository.existsById(employeeId)) {
+            Employee existingEmployee = employeeRepository.findById(employeeId).isPresent() ? employeeRepository.findById(employeeId).get() : null;
+
+            assert existingEmployee != null;
+            existingEmployee.setName(updatedEmployee.getName());
+            existingEmployee.setEmail(updatedEmployee.getEmail());
+            existingEmployee.setPosition(updatedEmployee.getPosition());
+            existingEmployee.setSalary(updatedEmployee.getSalary());
+
+            employeeRepository.save(existingEmployee);
+        } else {
+            throw new RuntimeException("Employee with id " + employeeId + " not found");
+        }
+    }
 }
